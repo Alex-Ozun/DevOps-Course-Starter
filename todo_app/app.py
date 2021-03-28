56e5b69183a5df8ae3e874c60bfc80757dda42f2
Alex Ozun <alex.ozun@aexp.com>
2021-03-28 20:16:07 +0100

    add form with inputs

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,18 +1,21 @@
-from flask import Flask
-from flask import render_template
+from flask import abort, Flask, render_template, request, redirect, url_for
 
-from todo_app.data.session_items import get_items
+from todo_app.data.session_items import get_items, add_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    items = get_items()
-    return render_template('index.html', items=items)
-
+    if request.method == 'GET':
+        items = get_items()
+        return render_template('index.html', items=items)
+    elif request.method == 'POST':
+        add_item(request.form['new_item'])
+        return redirect(url_for('index'))
+    else:
+        abort(405)
 
 if __name__ == '__main__':
     app.run()
